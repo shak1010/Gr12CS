48b4a3eb661434a21159a40d5c790956f22d85e5
shak1010 <shakeel.sumar@gmail.com>
2025-02-14 14:07:13 -0500

    solved lesson 3

--- lesson03/lesson3.py
+++ lesson03/lesson3.py
@@ -1,1 +1,11 @@
 # Lesson 3
+# Get input
+tiles = input("Enter the number of tiles: ")
+# Change to integer 
+tiles = int(tiles)
+
+print(f"Number of tiles: {tiles}")
+
+calc = int((tiles**0.5)//1)
+
+print(f"Answer is {calc}")
