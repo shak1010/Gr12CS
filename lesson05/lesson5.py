f978d76b5fee07b235813b0f7f8da636cc6e85ae
shak1010 <shakeel.sumar@gmail.com>
2025-02-20 14:11:39 -0500

    solved lesson 5

--- lesson05/lesson5.py
+++ lesson05/lesson5.py
@@ -1,1 +1,14 @@
 # Lesson 5
+money = int(input("Enter amount of money you started with: "))
+cookiename = input("Enter a string contaning b(large cookie) and c(normal cookie) ")
+
+numb = cookiename.count("b")
+numc = cookiename.count("c")
+
+print(f"Number of Normal cookies: {numb}")
+print(f"Number of Big cookies: {numc}")
+
+print(f"profit of normal cookie: {numb*(1.25-0.5)} ")
+print(f"profit of big cookie: {numb*(2.00-0.75)} ")
+
+print(f"You have {money - (numc*1.25 + numb*2.00)} dollars remaining")
