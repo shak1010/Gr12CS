16b82293ab968098671addccf29689f64489d1cb
shak1010 <shakeel.sumar@gmail.com>
2025-02-20 17:41:14 -0500

    Solved lesson 10

--- lesson10/lesson10.py
+++ lesson10/lesson10.py
@@ -1,1 +1,17 @@
 # Lesson 10
+four_digs = input("Enter the last 4 digits of the phone number calling you: ")
+count = 0
+if(four_digs[0] == "8" or four_digs[0] == "9"):
+    count = count + 1
+
+if(four_digs[2] == four_digs[1]):
+    count = count + 1
+
+if(four_digs[3] == "8" or four_digs[3] == "9"):
+    count = count + 1
+
+if(count == 3):
+    print("This is a telemarketer ")
+
+else: 
+    print("Not a telemarketer")
