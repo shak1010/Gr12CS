981e929808e15d802dabff5b078257e24b277b44
shak1010 <shakeel.sumar@gmail.com>
2025-02-20 14:27:06 -0500

    solved Lesson 7

--- lesson07/lesson7.py
+++ lesson07/lesson7.py
@@ -1,1 +1,9 @@
 # Lesson 7
+import random
+dc_value = int(input("enter value of dc (1-20): "))
+rolled_dice = random.randrange(1,20)
+
+if (dc_value > rolled_dice): 
+    print("you have failed!")
+else: 
+    print("You are sucessful!")
