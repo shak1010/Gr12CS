cf5e4495c276995cf05c9ed181b19ee2672e3cbb
shak1010 <shakeel.sumar@gmail.com>
2025-02-20 17:49:04 -0500

    Solved lesson 9

--- lesson09/lesson9.py
+++ lesson09/lesson9.py
@@ -1,1 +1,25 @@
 # Lesson 9
+from random import choice
+user = input("enter a choice of rock, paper or scissors: ")
+
+
+cpu = choice(["rock", "paper","scissors"])
+print(f"CPU choses {cpu}")
+
+if user not in ["rock", "paper", "scissors"]:
+    print("invalid choice")
+
+else: 
+    if (cpu == user):
+        print("Tie")
+
+    if (cpu == "rock"):
+        if(user == "scissors"):
+            print("CPU wins")
+        else:
+            print("You win")
+    elif(user == "rock"):
+        if(cpu == "paper"):
+            print("CPU wins")
+        else:
+            print("You win")
