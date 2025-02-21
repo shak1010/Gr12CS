6a0804e2aa89a43fe834fd861f0a450e429659b0
shak1010 <shakeel.sumar@gmail.com>
2025-02-21 01:16:46 -0500

    Factors Max

--- lesson21/lesson21.py
+++ lesson21/lesson21.py
@@ -1,1 +1,18 @@
 # Lesson 21
+
+N = int(input("Enter N input: "))
+
+max_factors = 0
+number_with_max_factors = 1
+
+for num in range(1, N + 1): 
+    factors = []  
+    for i in range(1, num + 1): 
+        if num % i == 0:
+            factors.append(i)  
+
+    if len(factors) > max_factors:
+        max_factors = len(factors)
+        number_with_max_factors = num
+
+print(f"Number with most factors: {number_with_max_factors} (Total Factors: {max_factors})")
