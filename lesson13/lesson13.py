f449a797c0f8ea9eacbabe160ee00b27ff464282
shak1010 <shakeel.sumar@gmail.com>
2025-02-21 00:06:03 -0500

    Lesson 13

--- lesson13/lesson13.py
+++ lesson13/lesson13.py
@@ -1,1 +1,19 @@
 # Lesson 13
+date = input("Enter the month (1-12) and day (1-31): ")
+date = list(map(int, date.split(' ')))
+
+month, day = date
+
+if(month == 2):
+    if(day == 18):
+        print(" Exact date of Feb 18th!")
+    elif(day > 18):
+        print("After Feb 18th")
+    else: 
+        print("Before Fb 18th")
+elif(month > 2):
+    print("After Feb 18th")
+
+else: 
+    print("Before Feb 18th")
+
