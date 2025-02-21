ab7c0e7b5e28295be34628bcb6dbc62b72f0ecce
shak1010 <shakeel.sumar@gmail.com>
2025-02-21 09:57:13 -0500

    Lesson 24

--- lesson23/lesson23.py
+++ lesson23/lesson23.py
@@ -1,1 +1,18 @@
-# Lesson 23
+user = 0
+total = 0
+counter = 0
+
+while user != -1:
+    user = int(input("Enter Value: (type -1 to stop) "))
+    
+    if user == -1:
+        break  
+
+    total += user
+    counter += 1
+
+
+if counter > 0:
+    print(f"{total / counter} is the average of your numbers")
+else:
+    print("No numbers were entered.")
--- lesson24/lesson24.py
+++ lesson24/lesson24.py
@@ -1,1 +1,22 @@
-# Lesson 24
+# lesson 24
+
+user = ""
+length = 0
+max_len = 0
+max_string = ""
+while(user != "x"):
+    user = input("Enter a string(type x to stop): ")
+    if (user == "x"):
+        break
+    
+    length = len(user)
+
+    if(max_len<length):
+        max_len = length
+        max_string = user
+
+
+print(f"{max_string} is the longest word with {max_len} characters")
+
+
+    
