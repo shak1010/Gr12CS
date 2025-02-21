a06dffb5ebe5c7488736ba0e4926da4ba878eceb
shak1010 <shakeel.sumar@gmail.com>
2025-02-21 00:15:43 -0500

    Lesson16

--- lesson16/lesson16.py
+++ lesson16/lesson16.py
@@ -1,1 +1,10 @@
 # Lesson 16
+for num in range(1, 51):  # Loop from 1 to 50
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
