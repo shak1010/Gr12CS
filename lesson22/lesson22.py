258983d9916f7bf01246d3d9fccbf4dc6becadda
shak1010 <shakeel.sumar@gmail.com>
2025-02-21 01:24:20 -0500

    Fibonacci

--- lesson22/lesson22.py
+++ lesson22/lesson22.py
@@ -1,1 +1,17 @@
 # Lesson 22
+
+# Straight to video lol
+
+upper_limit = int(input("Enter Nth num: "))
+
+fib_0 = 0
+fib_1 = 1
+fib_n = 0
+
+for n in range (2, upper_limit + 1):
+    fib_n = fib_1 + fib_0
+
+    fib_0 = fib_1
+    fib_1 = fib_n
+
+print(f"Fibonacci[{upper_limit}] is {fib_n}")
