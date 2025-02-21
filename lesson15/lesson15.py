b38a8b48c38031abba1b018258ed418ba3f77f8c
shak1010 <shakeel.sumar@gmail.com>
2025-02-21 00:12:28 -0500

    Differences B/W While For and Recursive

--- lesson15/lesson15.py
+++ lesson15/lesson15.py
@@ -1,1 +1,27 @@
 # Lesson 15
+
+# We can use while loops to complete repetitive tasks Genrate rane of values + express algorithms 
+# While depens on condition being true
+
+# Iterations DONT RELY On cond being truewill repeat finite of times u may control it
+
+'''
+def recursive_sum(num):
+ #Base Case
+    if num == 0:
+        return 0
+    else:
+        # Working towards the base case
+        return num + recursive_sum(num-1)
+        '''
+
+'''
+def fib(n):
+    if n == 0: # Base Case 1
+        return 0
+    elif n == 1: # Base Case 2
+        return 1
+    else:
+        # Working towards two base cases.
+        return fib(n-1) + fib(n-2)
+'''
