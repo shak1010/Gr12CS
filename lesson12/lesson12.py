4a96f6894413894f292e2a7b0f0eba8c72927a0d
shak1010 <shakeel.sumar@gmail.com>
2025-02-20 23:55:58 -0500

    Lesson12

--- lesson12/lesson12.py
+++ lesson12/lesson12.py
@@ -1,1 +1,19 @@
 # Lesson 12
+# My answer
+
+angles = input("Enter the repective angles: ")
+angles = list(map(int, angles.split()))
+
+a, b, c = angles
+
+if(sum(angles) != 180):
+    print("Invalid Input doesnt equal to 180")
+    exit(0)
+
+if(a == b == c):
+    print("Equilateral")
+
+elif(a==b or c==a  or b==c):
+    print("Isosceles")
+else:
+    print("Scalene")
