ff99095591fb50727d6e5c66536ab58013032a08
shak1010 <shakeel.sumar@gmail.com>
2025-02-21 01:08:12 -0500

    sum of 10 000 perfect num

--- lesson20/lesson20.py
+++ lesson20/lesson20.py
@@ -1,1 +1,14 @@
 # Lesson 20
+total_sum = 0
+
+for num in range(0, 10000): 
+    divisors_sum = 0
+
+    for i in range(1, num): 
+        if num % i == 0:
+            divisors_sum += i
+
+    if divisors_sum == num: 
+        total_sum += num
+
+print(f"Sum of all perfect numbers under 10,000: {total_sum}")
