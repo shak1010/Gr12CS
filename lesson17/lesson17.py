2ec38f5787a05e122153deb24d7097faf53fc62f
shak1010 <shakeel.sumar@gmail.com>
2025-02-21 00:21:32 -0500

    factorial

--- lesson17/lesson17.py
+++ lesson17/lesson17.py
@@ -1,1 +1,12 @@
 # Lesson 17
+
+num = int(input("Enter number: "))
+initial_num = num
+fact = num
+
+while(num>1): # number 1 doesnt matter as it will remain as same number
+    fact = fact*(num-1)
+
+    num-=1
+else:
+    print(f"Factorial of {initial_num} is: {fact}")
