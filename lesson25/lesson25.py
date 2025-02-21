6a200bc2ccbae9d467aeed792196bddd4477825f
shak1010 <shakeel.sumar@gmail.com>
2025-02-21 10:45:13 -0500

    Lesson 25

--- lesson25/lesson25.py
+++ lesson25/lesson25.py
@@ -1,1 +1,12 @@
-# Lesson 25
+num = int(input("Enter number: "))
+factor = 2
+largest_prime = 1
+
+while num > 1:
+    if num % factor == 0:
+        largest_prime = factor  
+        num //= factor  
+    else:
+        factor += 1  
+
+print(f"The largest prime factor is {largest_prime}")
