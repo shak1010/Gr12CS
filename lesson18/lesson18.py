df2ec102c3a665a92fa3baa2f24defdf189ae7e0
shak1010 <shakeel.sumar@gmail.com>
2025-02-21 00:58:15 -0500

    Factors

--- lesson18/lesson18.py
+++ lesson18/lesson18.py
@@ -1,1 +1,16 @@
 # Lesson 18
+
+num = int(input("Enter number: "))
+initial_num = num
+fact = num
+
+factors = ""
+
+while(num>0): # number 1 doesnt matter as it will remain as same number
+    if(fact%num == 0):
+        factors += f"{num}"
+        if(num != 1):
+            factors+= ","
+    num-=1
+else:
+    print(f"Factors of {initial_num} is: [{factors}]")
