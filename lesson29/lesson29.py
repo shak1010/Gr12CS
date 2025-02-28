50e26f9b63f44e07a011711885fed7d86febd49b
shak1010 <shakeel.sumar@gmail.com>
2025-02-28 14:53:47 -0500

    KevinIsTheGoat

--- lesson29/lesson29.py
+++ lesson29/lesson29.py
@@ -1,1 +1,13 @@
 # Lesson 29
+def consonant_counter(text):
+    counter = 0
+    text.lower()
+
+    for i in range (0, len(text)):
+        if(text[i].isalpha()):
+            if text[i] not in "aeiou":
+                counter+=1
+    
+    return counter
+
+print(consonant_counter(input("enter string: ")))
