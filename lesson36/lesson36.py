823092acd58211ac1b072b63486a5ab41bf779a1
shak1010 <shakeel.sumar@gmail.com>
2025-03-13 16:07:05 -0400

    Facotrs+Prime

--- lesson36/lesson36.py
+++ lesson36/lesson36.py
@@ -1,1 +1,18 @@
 # Lesson 36
+
+def factors(user):
+    temp = []
+    for i in range(1, user + 1): 
+        if user % i == 0:  
+            temp.append(i)
+    return temp 
+def prime(text):
+    if(len(factors(text)) != 2):
+        return "Not A Prime Number"
+    else:
+        return "A Prime Number"
+
+# Get user input and print factors
+num = int(input("Enter a number: "))
+print(f"Factors are {factors(num)}")
+print(f"Therefore the number is {prime(num)}")
