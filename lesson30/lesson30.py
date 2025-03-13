b71dff9f5228c566eeb7fff7c6cf3b8478798b63
shak1010 <shakeel.sumar@gmail.com>
2025-03-13 01:36:44 -0400

    Lesson30

--- lesson30/lesson30.py
+++ lesson30/lesson30.py
@@ -1,1 +1,11 @@
 # Lesson 30
+user = int(input("Enter number"))
+temp = ""
+i = 1
+while(i <= user):
+    if(i % 2 ==0):
+        temp += "0"
+    else:
+        temp += "1"
+    print(temp)
+    i += 1
