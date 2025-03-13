d669ae1a6e91e2e57e24f85ee76b7a3256a96a15
shak1010 <shakeel.sumar@gmail.com>
2025-03-12 22:51:45 -0400

    Lesson27

--- lesson27/lesson27.py
+++ lesson27/lesson27.py
@@ -1,1 +1,9 @@
 # Lesson 27
+def string_cleaner(text):
+    result = " "
+    for character in text:
+        if(character.isalpha()):
+            print(character, end = "")
+    return result
+
+string_cleaner(input("Enter word"))
