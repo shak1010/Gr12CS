baf4140f127714ae782470706c478f8b059e4b5b
shak1010 <shakeel.sumar@gmail.com>
2025-03-12 23:15:00 -0400

    PalindromCheck

--- lesson28/lesson28.py
+++ lesson28/lesson28.py
@@ -1,1 +1,11 @@
 # Lesson 28
+user = input()
+palindrome = True
+for i in range(len(user) // 2):
+    if(user[i] != user[(len(user)-1) - i]):
+        palindrome = False
+        print(f"{user} is not a palindrome")
+        break
+
+if (palindrome == True):
+    print(f"{user} is a palindrome")
