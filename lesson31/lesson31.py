b15507fb0f3c97457b145babac34b0f659d5afad
shak1010 <shakeel.sumar@gmail.com>
2025-03-13 15:02:14 -0400

    Anagram

--- lesson31/lesson31.py
+++ lesson31/lesson31.py
@@ -1,1 +1,13 @@
 # Lesson 31
+def anagrams_check(user1, user2):
+    if(len(user1) != len(user2)):
+        return "Not A Anagram"
+    if(sorted(user1) == sorted(user2)):
+        return "Anagram"
+    else:
+        return "Not A Anagram"
+
+word1 = input()
+word2 = input()
+
+print(anagrams_check(word1, word2))
