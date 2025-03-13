f1f6cee57b669018a938c7c036c837e26c976133
shak1010 <shakeel.sumar@gmail.com>
2025-03-13 15:21:57 -0400

    Set

--- lesson32/lesson32.py
+++ lesson32/lesson32.py
@@ -1,1 +1,18 @@
 # Lesson 32
+def duplicate_check(user1, user2):
+    sort = ""
+    user1 = sorted(set(user1)) # remove duplicates
+    user2 = sorted(set(user2))
+
+    for i in range(0, len(user1)):
+        for x in range(0, len(user2)):
+
+            if(user1[i] == user2[x]):
+                sort += user1[i]
+
+    return sort
+
+word1 = input()
+word2 = input()
+
+print(duplicate_check(word1, word2))
