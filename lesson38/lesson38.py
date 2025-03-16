ee9db82d79810fbc3bfe05bad9f6d914273ee035
shak1010 <shakeel.sumar@gmail.com>
2025-03-16 18:27:05 -0400

    Palindrome

--- lesson38/lesson38.py
+++ lesson38/lesson38.py
@@ -1,1 +1,14 @@
 # Lesson 38
+def pal(text):
+    return text == text[::-1] # slicing to reverse a slicable object
+
+pal_num = []
+
+for num1 in range(999,99,-1):
+    for num2 in range(999,99,-1):
+        cp = num1*num2
+
+        if pal(str(cp)):
+            pal_num.append(cp)
+
+print(max(pal_num))
