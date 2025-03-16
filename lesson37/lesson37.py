b42377acc198344a566fa1a22b6ba1a1d2f519cb
shak1010 <shakeel.sumar@gmail.com>
2025-03-16 02:57:15 -0400

    Compress

--- lesson37/lesson37.py
+++ lesson37/lesson37.py
@@ -1,1 +1,22 @@
 # Lesson 37
+def compress_string(s):
+    compressed = []
+    count = 1
+
+    for i in range(len(s) - 1):
+        if s[i] == s[i + 1]:
+            count += 1
+        else:
+            compressed.append(s[i] + str(count))
+            count = 1  # Reset counter for the next character
+
+
+    compressed.append(s[-1] + str(count))
+
+    compressed_str = "".join(compressed)
+    
+
+    return compressed_str if len(compressed_str) < len(s) else s
+
+
+print(compress_string(input()))  
