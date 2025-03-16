55bd2bc3b7dda6ac9417ee41146e50c954ad1b6d
shak1010 <shakeel.sumar@gmail.com>
2025-03-16 18:41:09 -0400

    GCD

--- lesson39/lesson39.py
+++ lesson39/lesson39.py
@@ -1,1 +1,11 @@
 # Lesson 39
+def gcd(num1, num2):
+    temp = []
+    for i in range(1, min(num1,num2)+1): # Only up to smaller num
+            if(num1 % i == 0 and num2 % i == 0):
+                temp.append(i)
+    return max(temp)
+    
+num1 = int(input())
+num2 = int(input())
+print(gcd(num1, num2))
