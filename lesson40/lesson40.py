ef3ec9a89f1c462b7c6c1a65c84a140958102ddd
shak1010 <shakeel.sumar@gmail.com>
2025-03-17 00:56:47 -0400

    Prime Check

--- lesson40/lesson40.py
+++ lesson40/lesson40.py
@@ -1,1 +1,16 @@
 # Lesson 40
+def factors(num):
+    i = 1
+    fact_list = []
+    while(num>=i):
+        if(num % i == 0):
+            fact_list.append(i)
+        i+=1
+    if(len(fact_list)!=2):
+        return "Not A prime Number"
+    else:
+        return "prime"
+
+num = int(input())
+print(factors(num))
+
