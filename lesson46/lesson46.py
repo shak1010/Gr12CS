db94a21c86ac8a8026ff4d43d84ac55d397c8746
shak1010 <shakeel.sumar@gmail.com>
2025-03-17 01:05:01 -0400

    lesson46

--- lesson46/lesson46.py
+++ lesson46/lesson46.py
@@ -1,1 +1,10 @@
 # Lesson 46
+user = int(input("Enter a number: "))
+
+while(user != 1):
+    if(user %2 == 0):
+        user = user//2
+        print(user)
+    else:
+        user = 3*user + 1
+        print(user)
