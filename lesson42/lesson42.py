8b991d1859b53b716e418a20c0d39dd3d7cc84e7
shak1010 <shakeel.sumar@gmail.com>
2025-03-19 00:03:37 -0400

    Possible Sum

--- lesson42/lesson42.py
+++ lesson42/lesson42.py
@@ -1,1 +1,14 @@
 # Lesson 42
+def possiblesum(a_list, target):
+    
+    for i, value in enumerate(a_list):
+        new_target = target - value
+
+        if new_target in a_list[i+1:]:
+            return True 
+    return False
+
+test = [1,3,5,8,12,13,22]
+target= 16
+
+print(possiblesum(test, target))
